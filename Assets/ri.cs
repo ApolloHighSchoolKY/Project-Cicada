aac722720d65178b465c98c801a23d13fe9ac389
Nicol Bolas <42354673+nbosley4401@users.noreply.github.com>
2018-10-03 10:31:13 -0500

    v0.0.3.2
    Added comments

--- Assets/ri.cs
+++ Assets/ri.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class ri : MonoBehaviour {
 
-    public Rigidbody rb;
-    int x1;
+    public Rigidbody rb; //Rigidbody to apply forces to
+	//Variables to store force values in
+	int x1;
     int y1;
     int z1;
 
@@ -17,13 +18,15 @@ public class ri : MonoBehaviour {
 
     // Update is called once per frame
     private void Update()
-    {
+    {	
+		//Generate random values for x,y,z axis forces
         x1 = Random.Range(-1500, 1500);
         y1 = Random.Range(-1500, 1500);
         z1 = Random.Range(-250, 1500);
     }
     void FixedUpdate ()
     {
+		//Applies random forces to rigidbody
         rb.AddForce(x1*Time.deltaTime, y1*Time.deltaTime, z1* Time.deltaTime);
 	}
 }
