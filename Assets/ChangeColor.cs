21ce51438c7df74c596df9fc718fbcc94c6f7608
Nicol Bolas <42354673+nbosley4401@users.noreply.github.com>
2018-10-03 10:12:18 -0500

    v0.0.3.1.2
    Added comments to code

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
+    //Variable to store (r,g,b,a) values)
     byte rndR;
     byte rndG;
     byte rndB;
@@ -16,10 +17,13 @@ public class ChangeColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Sets variable to random vaule between 0 and 255
         rndR = (byte)Random.Range(0, 255);
         rndG = (byte)Random.Range(0, 255);
         rndB = (byte)Random.Range(0, 255);
         rndA = (byte)Random.Range(0, 255);
+        
+        //Changes color of object to the stored (r,g,b,a) values
         gameObject.GetComponent<Renderer>().material.color = new Color32(rndR, rndG, rndB, rndA);
     }
 }
