111d999563266ebc5ae0de10fb76b1aa490e9401
Nicol Bolas <42354673+nbosley4401@users.noreply.github.com>
2018-10-29 12:16:41 -0500

    Added targets
    -Added targets to demo map
    Changes
         -Increased size of bullets for demo

---
+++ Assets/Respawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+    public GameObject self;
+    public int startingLife;
+	// Use this for initialization
+	void Start () {
+        startingLife = GetComponent<Health>().life;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {   GetComponent<Health>().life = startingLife;
+            self.SetActive(true);
+        }
+    }
+}
